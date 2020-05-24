c297db9e2478c5f1b43db4ce3f22eed3619797e4
Waffle <waffle.lapkin@gmail.com>
2020-05-24 09:45:36 +0300

    Clean fraction module
    - Write more documentation
    - Change pretty-display output to be prettier
    - Move `Mul` impls higher than `Div` impls

--- src/fraction.rs
+++ src/fraction.rs
@@ -1,6 +1,5 @@
 use core::{
     fmt::{self, Write},
-    marker::PhantomData,
     ops::{Div, Mul},
 };
 
@@ -8,17 +7,51 @@ use typenum::{Prod, UInt, Unsigned, U0, U1};
 
 use crate::{eq::FractionEq, from_int::FromUnsigned};
 
-/// Fraction `Numerator / Denominator`
-pub struct Fraction<Numerator, Denominator>(PhantomData<(Numerator, Denominator)>);
+/// **Type-level** fraction `Numerator / Denominator`. It's primarily used for
+/// ratio. See also: [`Frac!`](crate::Frac) macro.
+///
+/// ## Examples
+///
+/// ```
+/// use typed_phy::{Frac, simplify::Simplify};
+/// use typenum::{Prod, assert_type_eq, U2, U3, U5, U7, U14, U15, U30, U42};
+///
+/// assert_type_eq!(Prod<Frac![U2 / U5], Frac![U7 / U3]>, Frac![U14 / U15]);
+/// assert_type_eq!(<Frac![U30 / U42] as Simplify>::Output, Frac![U5 / U7]);
+/// ```
+///
+/// ```
+/// use typed_phy::Frac;
+/// use typenum::{U0, U1, U2, U3, U5, U7};
+///
+/// // Note that `Fraction` implements both `Debug` and `Display`
+/// // and also uses the alternate (`#`) flag in display.
+///
+/// assert_eq!(format!("{:?}", <Frac![U2 / U3]>::new()), "Fraction<2/3>");
+/// assert_eq!(format!("{:?}", <Frac![U5 / U5]>::new()), "Fraction<5/5>");
+/// assert_eq!(format!("{:?}", <Frac![U0 / U7]>::new()), "Fraction<0/7>");
+/// assert_eq!(format!("{:?}", <Frac![U3 / U1]>::new()), "Fraction<3/1>");
+///
+/// assert_eq!(format!("{}", <Frac![U2 / U3]>::new()), "2/3");
+/// assert_eq!(format!("{}", <Frac![U5 / U5]>::new()), "5/5");
+/// assert_eq!(format!("{}", <Frac![U0 / U7]>::new()), "0/7");
+/// assert_eq!(format!("{}", <Frac![U3 / U1]>::new()), "3/1");
+///
+/// assert_eq!(format!("{:#}", <Frac![U2 / U3]>::new()), "2 / 3");
+/// assert_eq!(format!("{:#}", <Frac![U5 / U5]>::new()), "1");
+/// assert_eq!(format!("{:#}", <Frac![U0 / U7]>::new()), "0");
+/// assert_eq!(format!("{:#}", <Frac![U3 / U1]>::new()), "3");
+/// ```
+pub struct Fraction<Numerator, Denominator>(phantasm::Invariant<(Numerator, Denominator)>);
 
-/// Default fraction. `1/1`
+/// One, `1 / 1`.
 pub type One = Fraction<U1, U1>;
 
 impl<N, D> Fraction<N, D> {
     /// Create new fraction
     #[inline]
     pub const fn new() -> Self {
-        Self(PhantomData)
+        Self(phantasm::Invariant)
     }
 }
 
@@ -29,13 +62,19 @@ impl<N, D> Default for Fraction<N, D> {
     }
 }
 
-///
+/// Helper trait for [`Fraction`](Fraction)
 pub trait FractionTrait {
-    ///
+    /// The numerator of the fraction
     type Numerator: Unsigned;
 
-    ///
+    /// The divisor of the fraction
     type Divisor: Unsigned;
+
+    // Note: I would like to remove mul/div and instead use Mul/Div traits, but I
+    // can't make both       `impl<T: FromInteger + ...> Mul/Div<T> for
+    // Fraction<>` and all the type level Mul/Divs       at the same time. It is
+    // possible to implement this for all `i*` and `u*` but that's       boring
+    // & won't allow using storage types other than std's..................
 
     /// Multiply integer by this fraction
     ///
@@ -83,19 +122,6 @@ where
     type Numerator = N;
 }
 
-/// `(n/d) / x = n/(d * x)`
-impl<N, D, X, B> Div<UInt<X, B>> for Fraction<N, D>
-where
-    D: Mul<UInt<X, B>>,
-{
-    type Output = Fraction<N, Prod<D, UInt<X, B>>>;
-
-    #[inline]
-    fn div(self, _rhs: UInt<X, B>) -> Self::Output {
-        Self::Output::new()
-    }
-}
-
 /// `(n/d) * x = (n * x)/d`
 impl<N, D, X, B> Mul<UInt<X, B>> for Fraction<N, D>
 where
@@ -105,6 +131,19 @@ where
 
     #[inline]
     fn mul(self, _rhs: UInt<X, B>) -> Self::Output {
+        Self::Output::new()
+    }
+}
+
+/// `(n/d) / x = n/(d * x)`
+impl<N, D, X, B> Div<UInt<X, B>> for Fraction<N, D>
+where
+    D: Mul<UInt<X, B>>,
+{
+    type Output = Fraction<N, Prod<D, UInt<X, B>>>;
+
+    #[inline]
+    fn div(self, _rhs: UInt<X, B>) -> Self::Output {
         Self::Output::new()
     }
 }
@@ -122,20 +161,6 @@ where
     }
 }
 
-/// `(n/d) / (a/b) = (n * b)/(d * a)`
-impl<N, D, A, B> Div<Fraction<A, B>> for Fraction<N, D>
-where
-    N: Mul<B>,
-    D: Mul<A>,
-{
-    type Output = Fraction<Prod<N, B>, Prod<D, A>>;
-
-    #[inline]
-    fn div(self, _rhs: Fraction<A, B>) -> Self::Output {
-        Self::Output::new()
-    }
-}
-
 /// `(n/d) * (a/b) = (n * a)/(d * b)`
 impl<N, D, A, B> Mul<Fraction<A, B>> for Fraction<N, D>
 where
@@ -146,6 +171,20 @@ where
 
     #[inline]
     fn mul(self, _rhs: Fraction<A, B>) -> Self::Output {
+        Self::Output::new()
+    }
+}
+
+/// `(n/d) / (a/b) = (n * b)/(d * a)`
+impl<N, D, A, B> Div<Fraction<A, B>> for Fraction<N, D>
+where
+    N: Mul<B>,
+    D: Mul<A>,
+{
+    type Output = Fraction<Prod<N, B>, Prod<D, A>>;
+
+    #[inline]
+    fn div(self, _rhs: Fraction<A, B>) -> Self::Output {
         Self::Output::new()
     }
 }
@@ -202,7 +241,7 @@ where
         } else {
             // TODO: use gcd here?...
             f.write_fmt(format_args!(
-                "{numerator}/{divisor}",
+                "{numerator} / {divisor}",
                 numerator = numerator,
                 divisor = divisor,
             ))
@@ -242,6 +281,9 @@ mod tests {
         assert_eq!(format!("{:#}", <Frac![U100 / U1]>::new()), "100");
         assert_eq!(format!("{:#}", <Frac![U3 / U3]>::new()), "1");
         assert_eq!(format!("{:#}", <Frac![U0 / U3]>::new()), "0");
-        assert_eq!(format!("{:#}", <Frac![U1000 / U3600]>::new()), "1000/3600");
+        assert_eq!(
+            format!("{:#}", <Frac![U1000 / U3600]>::new()),
+            "1000 / 3600"
+        );
     }
 }
